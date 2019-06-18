e693cc7dc15097d345e8393b9e9f876d30ee2e59
qiudekai026 <1297637494@qq.com>
2019-06-18 09:18:35 +0800

    创建4个主要的界面

--- app/src/main/java/cn/edu/gdpt/yinyuan171026qdk/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/yinyuan171026qdk/MainActivity.java
@@ -1,13 +1,91 @@
 package cn.edu.gdpt.yinyuan171026qdk;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<Fragment> fragmentList;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout1);
+        tabLayout.addTab(tabLayout.newTab().setText("首页 ").setIcon(R.drawable.icon_certificate));
+        tabLayout.addTab(tabLayout.newTab().setText("消息 ").setIcon(R.drawable.icon_message));
+        tabLayout.addTab(tabLayout.newTab().setText("动态 ").setIcon(R.drawable.icon_circle));
+        tabLayout.addTab(tabLayout.newTab().setText("我 ").setIcon(R.drawable.icon_signal));
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new SouyeFragment());
+        fragmentList.add(new DonTaiFragment());
+        fragmentList.add(new XiaoxiFragment());
+        fragmentList.add(new MeFragment());
+        MyVPFragmentAdapter myVPFragmentAdapter = new MyVPFragmentAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.vp);
+        viewPager.setAdapter(myVPFragmentAdapter);
+
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                tabLayout.getTabAt(i).select();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+
+
+        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                    if (tab == tabLayout.getTabAt(i))
+                        viewPager.setCurrentItem(i);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    class MyVPFragmentAdapter extends FragmentPagerAdapter {
+        public MyVPFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
     }
 }
+

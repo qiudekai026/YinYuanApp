851bfbe98d422ababbb03a29d20bd5603b82ea19
1711605024 <17817388438@qq.com>
2019-06-24 10:10:29 +0800

    首页图片滑动

--- app/src/main/java/cn/edu/gdpt/yinyuan171026qdk/fragment/SouyeFragment.java
+++ app/src/main/java/cn/edu/gdpt/yinyuan171026qdk/fragment/SouyeFragment.java
@@ -2,10 +2,21 @@ package cn.edu.gdpt.yinyuan171026qdk.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import cn.edu.gdpt.yinyuan171026qdk.R;
 
@@ -14,6 +25,11 @@ import cn.edu.gdpt.yinyuan171026qdk.R;
  * A simple {@link Fragment} subclass.
  */
 public class SouyeFragment extends Fragment {
+    private ImageView[] imageViews = null;
+    private ImageView imageView = null;
+    private ViewPager advPager = null;
+    private AtomicInteger what = new AtomicInteger(0);
+    private boolean isContinue = true;
 
 
     public SouyeFragment() {
@@ -25,7 +41,181 @@ public class SouyeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_souye, container, false);
+        View view = inflater.inflate(R.layout.fragment_souye, container, false);
+        initViewPager(view);
+        return view;
+    }
+
+    private void initViewPager(View view) {
+        advPager = (ViewPager) view.findViewById(R.id.adv_pager);
+        ViewGroup group = (ViewGroup) view.findViewById(R.id.viewGroup);
+
+        List<View> advPics = new ArrayList<View>();
+        ImageView img1 = new ImageView(getActivity());
+        img1.setBackgroundResource(R.drawable.a1);
+        advPics.add(img1);
+
+        ImageView img2 = new ImageView(getActivity());
+        img2.setBackgroundResource(R.drawable.a2);
+        advPics.add(img2);
+
+        ImageView img3 = new ImageView(getActivity());
+        img3.setBackgroundResource(R.drawable.a3);
+        advPics.add(img3);
+
+        ImageView img4 = new ImageView(getActivity());
+        img4.setBackgroundResource(R.drawable.a4);
+        advPics.add(img4);
+
+        imageViews = new ImageView[advPics.size()];
+        for (int i = 0; i < advPics.size(); i++) {
+            imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(20, 20));
+            imageView.setPadding(5, 5, 5, 5);
+            imageViews[i] = imageView;
+            if (i == 0) {
+                imageViews[i]
+                        .setBackgroundResource(R.drawable.left);
+            } else {
+                imageViews[i]
+                        .setBackgroundResource(R.drawable.right);
+            }
+            group.addView(imageViews[i]);
+        }
+
+        advPager.setAdapter(new AdvAdapter(advPics));
+        advPager.setOnPageChangeListener(new GuidePageChangeListener());
+        advPager.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_MOVE:
+                        isContinue = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        isContinue = true;
+                        break;
+                    default:
+                        isContinue = true;
+                        break;
+                }
+                return false;
+            }
+        });
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    if (isContinue) {
+                        viewHandler.sendEmptyMessage(what.get());
+                        whatOption();
+                    }
+                }
+            }
+
+        }).start();
+    }
+
+    private void whatOption() {
+        what.incrementAndGet();
+        if (what.get() > imageViews.length - 1) {
+            what.getAndAdd(-4);
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    private final Handler viewHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            advPager.setCurrentItem(msg.what);
+            super.handleMessage(msg);
+        }
+
+    };
+
+    private final class GuidePageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageSelected(int arg0) {
+            what.getAndSet(arg0);
+            for (int i = 0; i < imageViews.length; i++) {
+                imageViews[arg0]
+                        .setBackgroundResource(R.drawable.left);
+                if (arg0 != i) {
+                    imageViews[i]
+                            .setBackgroundResource(R.drawable.right);
+                }
+            }
+        }
+    }
+
+    private final class AdvAdapter extends PagerAdapter {
+        private List<View> views = null;
+
+        public AdvAdapter(List<View> views) {
+            this.views = views;
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView(views.get(arg1));
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public Object instantiateItem(View arg0, int arg1) {
+            ((ViewPager) arg0).addView(views.get(arg1), 0);
+            return views.get(arg1);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+
+        }
+
     }
 
 }
+
